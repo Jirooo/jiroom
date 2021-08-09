454ab429564d1b3c88bbda3ee15e5ac9a477c9cc
Jirooo <50818022+Jirooo@users.noreply.github.com>
2021-08-10 04:04:54 +0900

    Add files via upload

---
+++ Jiroom/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/', methods=['GET','POST'])
+def top():
+    return render_template('top.html')
+
+@app.route('/math')
+def math():
+    return render_template('math.html')
+
+@app.route('/complex1')
+def complex1():
+    return render_template('complex1.html')
+
+@app.route('/complex2')
+def complex2():
+    return render_template('complex2.html')
+
+@app.route('/complex3')
+def complex3():
+    return render_template('complex3.html')
+
+@app.route('/complex4')
+def complex4():
+    return render_template('complex4.html')
+
+@app.route('/complex5')
+def complex5():
+    return render_template('complex5.html')
+
+@app.route('/complex6')
+def complex6():
+    return render_template('complex6.html')
+
+@app.route('/machineLearning1')
+def machineLearning1():
+    return render_template('machineLearning1.html')
+
+
+
+def main():
+    app.debug = True
+    app.run()
+
+if __name__ == '__main__':
+    main()
