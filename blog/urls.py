de52b9ff823f3caa094cd7c82ec922ed40457344
Jirooo <50818022+Jirooo@users.noreply.github.com>
2022-05-25 15:24:57 +0900

    Delete blog directory

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from blog.views import PostList, PostDetail, top
-
-urlpatterns = [
-    path("", top, name="top"),
-    path("blog", PostList.as_view(), name="blog"),
-    path("article/<int:pk>/", PostDetail.as_view(), name="article"),
-]
